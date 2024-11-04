20489f84335279418deaa1d39bbd9e13d4877e5f
Timon Erhart <57718207+turbotimon@users.noreply.github.com>
2024-11-04 09:17:30 +0000

    add comment to build script

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+
+# This script is intended to run in netifly as a build command (site config -> build & deploy -> build settings -> build command)
 set -e
 python -m venv .venv_tmp
 . .venv_tmp/bin/activate
